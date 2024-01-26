922410bfb5f566952f7da83877d482e557d84515
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-26 22:17:54 +0300

    Add files via upload

---
+++ src/pages/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatBox from "../components/ChatBox";
+import HeaderSection from "../components/HeaderSection";
+
+export default function Chat() {
+    return(
+        <>
+            <HeaderSection height={"10vh"}/>
+            <ChatBox />
+        </>
+    );
+}
