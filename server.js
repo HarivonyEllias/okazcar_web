f351175fbdc473f25ec517c5ea6cf3626d3a7d0b
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-02-09 22:59:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join("/opt/render/project/build/")));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join("/opt/render/project/build/index.html"));
 });
 console.log(__dirname);
 
